7ebab43db2a0e98c16b83afcf19ceeda8919f90f
jadanov89 <74437337+jadanov89@users.noreply.github.com>
2023-10-10 13:46:50 +0000

    Deploying to gh-pages from @ jadanov89/burning-man@714ca3911edc30bd1b63fb0d0379715ac872ac36 🚀

---
+++ index.3239ce82.js
@@ -0,0 +1,8 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-mobile-open]"),closeModalBtn:document.querySelector("[data-mobile-close]"),modal:document.querySelector("[data-menu]"),burgerIcon:document.querySelector("#icon-burger"),crossIcon:document.querySelector("#icon-cross")};function o(){e.modal.classList.toggle("is-open");let o=e.modal.classList.contains("is-open");o?(e.burgerIcon.style.display="none",e.crossIcon.style.display="block"):(e.burgerIcon.style.display="block",e.crossIcon.style.display="none")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();// const bookButton = document.querySelector(".bookButton");
+// bookButton.addEventListener("click", function() {
+//     this.disabled = true;
+//     this.disabled = false;
+// });
+//# sourceMappingURL=index.3239ce82.js.map
+
+//# sourceMappingURL=index.3239ce82.js.map
