776d524e8b6d8f03a549067a85a6ad3d17d23b4c
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-10-23 16:04:30 +0300

    slider

--- src/js/gallary.js
+++ src/js/gallary.js
@@ -1,24 +1,9 @@
 import PixabayApiService from './api';
 
 const pixabayApiService = new PixabayApiService();
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.swiper-wrapper');
 
-async function searchAndDisplayImages() {
-  try {
-    const images = await pixabayApiService.fetchImages('concert', 3);
-
-    if (images.length > 0) {
-      const searchResult = generateImagesMarkup(images);
-      galleryEl.insertAdjacentHTML('beforeend', searchResult);
-    } else {
-      console.log('Зображення не знайдено.');
-    }
-  } catch (error) {
-    console.error('Помилка при отриманні зображень:', error);
-  }
-}
-
-function createImageCard({
+const createImageCard = ({
   webformatURL,
   largeImageURL,
   tags,
@@ -26,9 +11,9 @@ function createImageCard({
   views,
   comments,
   downloads,
-}) {
-  return `<div class="photo-card">
-    <a href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
+}) => {
+  return `<div class="swiper-slide">
+    <a href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" data-source="${largeImageURL}" /></a>
     <div class="info">
       <p class="info-item">
         <b>Likes<br /><span class="stats">${likes}</span></b>
@@ -44,10 +29,72 @@ function createImageCard({
       </p>
     </div>
   </div>`;
-}
+};
 
-function generateImagesMarkup(imagesArray) {
+const generateImagesMarkup = (imagesArray) => {
   return imagesArray.reduce((acc, image) => acc + createImageCard(image), '');
+};
+
+async function searchAndDisplayImages() {
+  try {
+    const images = await pixabayApiService.fetchImages('concert', 10);
+
+    if (images.length > 0) {
+      const searchResult = generateImagesMarkup(images);
+      galleryEl.innerHTML = searchResult;
+    } else {
+      console.log('Зображення не знайдено.');
+    }
+  } catch (error) {
+    console.error('Помилка при отриманні зображень:', error);
+  }
 }
 
 searchAndDisplayImages();
+
+// Додамо mySwiper
+document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
+
+// Додамо обробник подій для відкриття зображення через basicLightbox
+galleryEl.addEventListener('click', galleryImageClick);
+
+window.addEventListener("keydown", closeModal);
+
+
+function galleryImageClick(evt) {
+  evt.preventDefault();
+
+    const isGalerryClick = evt.target.classList.contains("gallery__image");
+
+    if(!isGalerryClick) {
+      return;
+    }
+
+  const bigSizeImage = basicLightbox.create(
+    `<img src=${evt.target.dataset.source}>`
+  );
+
+  bigSizeImage.show();
+}
+
+function closeModal(evt) {
+  if (evt.code === "Escape") {
+    fullSizeImage.close(() =>
+      window.removeEventListener("keydown", closeModal)
+    );
+  }
+}
+  
