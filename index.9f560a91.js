e140838173517d3411fa9d227dc49e85b00185bf
jadanov89 <74437337+jadanov89@users.noreply.github.com>
2023-10-30 13:08:22 +0000

    Deploying to gh-pages from @ jadanov89/burning-man@71948a834ac25054a3d433996cd52b32f1a7c115 🚀

--- index.9f560a91.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){let e;let t=document.querySelector(".bookButton"),o=()=>{e=basicLightbox.create(`
-            <div class="modal">
-                <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-                <input placeholder="Type something">
-                <a id="closeModalButton">Close</a>
-            </div>
-        `,{onShow:e=>{let t=e.element().querySelector("#closeModalButton");t.addEventListener("click",()=>e.close()),document.addEventListener("keydown",n)},onClose:()=>{document.removeEventListener("keydown",n)}})},n=t=>{"Escape"===t.key&&e.close()};t.addEventListener("click",()=>{o(),e.show()})});//# sourceMappingURL=index.9f560a91.js.map
-
-//# sourceMappingURL=index.9f560a91.js.map
