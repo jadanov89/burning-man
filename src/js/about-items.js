aa46fe097a13e2d3d0477cdda528446e5d40bee8
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-10-30 17:44:43 +0200

    form

--- src/js/about-items.js
+++ src/js/about-items.js
@@ -1,7 +1,7 @@
 const aboutItems = [
     {
       title:
-        'Rebuilding the society',
+        `Rebuilding the society`,
       text:
         'Burning Man is a network of people inspired by the values reflected in the Ten Principles and united in the pursuit of a more creative and connected existence in the world. Throughout the year we work to build Black Rock City, home of the largest annual Burning Man gathering, and nurture the distinctive culture emerging from that experience.',
       
