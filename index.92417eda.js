f6e4edcddec6e7a44e73410a88c6683f4cb4daaa
jadanov89 <74437337+jadanov89@users.noreply.github.com>
2023-11-08 16:35:02 +0000

    Deploying to gh-pages from @ jadanov89/burning-man@ee382214c3f29c404ada3f0e0f7982d42dfa134d 🚀

--- index.92417eda.js
+++ index.92417eda.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded",function(){let e;let t=document.que
             <input type="phone" name="phone" placeholder="Your Phone" />
             <input type="email" name="email"  placeholder="Email Address" />
             <textarea name="text" placeholder="Type your Message"></textarea>
-            <button type="button">Send</button>
+            <button type="submit">Send</button>
         </form>
         </div>
-        `,{onShow:e=>{let t=e.element().querySelector(".closeModalButton");t.addEventListener("click",()=>e.close()),document.addEventListener("keydown",n)},onClose:()=>{document.removeEventListener("keydown",n)}})},n=t=>{"Escape"===t.key&&e.close()};t.addEventListener("click",()=>{o(),e.show()})});//# sourceMappingURL=index.29a731b2.js.map
+        `,{onShow:e=>{let t=e.element().querySelector(".closeModalButton");t.addEventListener("click",()=>e.close()),document.addEventListener("keydown",n)},onClose:()=>{document.removeEventListener("keydown",n)}})},n=t=>{"Escape"===t.key&&e.close()};t.addEventListener("click",()=>{o(),e.show()})});//# sourceMappingURL=index.92417eda.js.map
 
-//# sourceMappingURL=index.29a731b2.js.map
+//# sourceMappingURL=index.92417eda.js.map
