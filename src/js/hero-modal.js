c7d9b48ece4ed9206346d9f9ca94112cb29d0f38
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-11-10 17:27:52 +0200

    validate

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,40 +1,90 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btnModal = document.querySelector('.bookButton');
     let instance;
-
-    const createModal = () => {
-        instance = basicLightbox.create(`
-        <div class="modal"> 
-            <button type="button" class="closeModalButton"></button> 
-            <h1>Contact Us</h1>    
-            <form id="myForm">
-            <input type="name" name="name" placeholder="Your Name" />
-            <input type="phone" name="phone" placeholder="Your Phone" />
-            <input type="email" name="email"  placeholder="Email Address" />
-            <textarea name="text" placeholder="Type your Message"></textarea>
-            <button type="submit">Send</button>
-        </form>
-        </div>
-        `, {
-            onShow: (instance) => {
-                const closeModalButton = instance.element().querySelector('.closeModalButton');
-                closeModalButton.addEventListener('click', () => instance.close());
-                document.addEventListener('keydown', handleEscPress);
-            },
-            onClose: () => {
-                document.removeEventListener('keydown', handleEscPress);
-            },
+        
+        const createModal = () => {
+            instance = basicLightbox.create(`
+                <div class="modal"> 
+                    <button type="button" class="closeModalButton"></button> 
+                    <h1>Contact Us</h1>    
+                    <form id="myForm">
+                        <input type="text" name="name" placeholder="Your Name" />
+                        <input type="tel" name="phone" placeholder="Your Phone" />
+                        <input type="email" name="email" placeholder="Email Address" />
+                        <textarea name="text" placeholder="Type your Message"></textarea>
+                        <button type="submit" id="sendButton">Send</button>
+                    </form>
+                </div>
+                `, {
+                    onShow: (instance) => {
+                        const closeModalButton = instance.element().querySelector('.closeModalButton');
+                        closeModalButton.addEventListener('click', () => instance.close());
+                        document.addEventListener('keydown', handleEscPress);
+        
+                        // Додаємо обробник для кнопки "Send"
+                        const sendButton = instance.element().querySelector('#sendButton');
+                        sendButton.addEventListener('click', function () {
+                            validateForms('#myForm');
+                        });
+                    },
+                    onClose: () => {
+                        document.removeEventListener('keydown', handleEscPress);
+                    },
+                });
+            };
+        
+            const handleEscPress = (event) => {
+                if (event.key === 'Escape') {
+                    instance.close();
+                }
+            };
+        
+            btnModal.addEventListener('click', () => {
+                createModal();
+                instance.show();
+            });
+        
+            // Додамо валідацію форми після її створення
+            createModal();
         });
-    };
 
-    const handleEscPress = (event) => {
-        if (event.key === 'Escape') {
-            instance.close();
-        }
-    };
-
-    btnModal.addEventListener('click', () => {
-        createModal();
-        instance.show();
+function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required digits",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: "Введите {0} символа!"
+                },
+                phone: {
+                    required: "Пожалуйста, введите свой номер телефона",
+                    digits: "Только цифры разрешены"
+                },
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+                },
+            submitHandler: function (form) {
+                // Цей код буде викликано при успішній валідації
+                // Ви можете додати тут код для відправлення форми
+                alert('Форма успішно валідована і готова до відправлення!');
+            },
+            errorPlacement: function (error, element) {
+                // Встановлення місця відображення повідомлення про помилку
+                error.insertBefore(element);
+            }
     });
-});
+}
+        
+        
